c6efbd0fc55e1c20f0e4da85af758b9bbe53066b
debzkie05 <johndavidpitogo@gmail.com>
2026-05-06 17:36:09 +0800

    security: move Firebase credentials to env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRcYZTGaS7EDowSkIz86rsDGaoeEpCTNI",
-  authDomain: "student-reminder-c5438.firebaseapp.com",
-  projectId: "student-reminder-c5438",
-  storageBucket: "student-reminder-c5438.firebasestorage.app",
-  messagingSenderId: "540973913270",
-  appId: "1:540973913270:web:c0f053cf8a014de85e8217",
-  measurementId: "G-T9WEM62GXJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
